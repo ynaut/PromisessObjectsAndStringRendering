5426cf51dcf5a7d897075e37ca337714e0917687
Mauro Mandracchia <mauro@insided.nl>
2017-02-05 13:24:59 +0100

    Better syntax

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 ;(function(w, $){
 
   // Expecting
-  new GitHubUser('ideabile').getUserInformations().done(function ( informations ){
+  new GitHubUser('ideabile')
+    .getUserInformations()
+    .done(function ( informations ){
     this.user = informations;
-    this.getRepos().done(function( repos ){
+      return this.getRepos();
+    }).done(function( repos ){
       this.repos = repos;
       Render(this.render());
     });
-  });
 
 })(window, jQuery);
