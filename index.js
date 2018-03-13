1c436b8414593fdc620a4c67201b62e034a7fbdb
ynaut <eskil.petersson@gmail.com>
2018-03-13 23:42:49 +0100

    es6 solution

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 // The goal is render a github informations on a page with:
 // Promises, Map and Reduce.
 const API_GIT_USERS = 'https://api.github.com/users/'
-function GitHubUser (username) {
-    this.username = username;
-}
 
-  
-GitHubUser.prototype.getUserInformation = function () {
-    const username = this.username
-    return new Promise(function(resolve, reject) {
+class GitHubUser {
+    constructor(username) {
+        this.username = username;
+    }
+    getUserInformation() {
+        return new Promise((resolve, reject) =>  {
         const xhr = new XMLHttpRequest();
-        xhr.open("GET", `https://api.github.com/users/${username}`, true);
+        xhr.open("GET", `https://api.github.com/users/${this.username}`, true);
         xhr.onload = function (e) {
                 resolve( JSON.parse(xhr.responseText) )
           };
@@ -19,41 +18,40 @@ GitHubUser.prototype.getUserInformation = function () {
           };
           xhr.send();
       });
-};
-
-GitHubUser.prototype.getRepos = function () {
-    const username = this.username
-    return new Promise(function(resolve, reject) {
-        const xhr = new XMLHttpRequest();
-        xhr.open("GET", `https://api.github.com/users/${username}/repos`, true);
-        xhr.onload = function (e) {
-                resolve(JSON.parse(xhr.responseText))
-          };
-          xhr.onerror = function (e) {
-            reject(xhr.statusText);
-          };
-          xhr.send();
-      });
-}
-
-GitHubUser.prototype.render = function () {
-    let html = ''
-    const userView = `
-    <div>
-        <h1>${this.user.login}</h1>
-        <img src="${this.user.avatar_url}">
-    </div>`
-    
-    html += userView
-    html += this.repos.map( repo => repo.name)
-    .reduce( (x,y) =>  {
-        return x + `<li>${y}</li>`
-    },'')
-    return html
+    }
+    getRepos() {
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.open("GET", `https://api.github.com/users/${this.username}/repos`, true);
+            xhr.onload = function (e) {
+                    resolve(JSON.parse(xhr.responseText))
+              };
+              xhr.onerror = function (e) {
+                reject(xhr.statusText);
+              };
+              xhr.send();
+          });
+    }
+    render () {
+        let html = ''
+        const userView = `
+            <div>
+                <h1>${this.user.login}</h1>
+                <img src="${this.user.avatar_url}">
+            </div>`
+        
+        html += userView
+        html += this.repos
+        .map( repo => repo.name)
+        .reduce( (x,y) =>  {
+            return x + `<li>${y}</li>`
+        },'')
+        return html
+    }
 }
 
 function Render($element, html){
-    document.getElementsByClassName($element).innerHTML = html
+    document.getElementById($element).innerHTML = html
 }
 
 
